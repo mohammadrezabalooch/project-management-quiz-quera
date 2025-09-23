11e27aac531476bb92d43617e7fe859c32e28e76
MohammadReza <baloochmohammadreza@gmail.com>
2025-09-23 19:56:14 +0330

    serializer for first try...

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
+from .models import Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "category",
+            "members",
+            "created_at",
+            "updated_at",
+        ]
