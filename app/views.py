51d67f6837b9f160a714cf632c91a1de0a1b4281
MohammadReza <baloochmohammadreza@gmail.com>
2025-09-23 20:08:44 +0330

    views for first try

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from rest_framework.generics import ListAPIView
+from .serializers import ProjectSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Project
 
 
 class ProjectListView(ListAPIView):
-    pass
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
